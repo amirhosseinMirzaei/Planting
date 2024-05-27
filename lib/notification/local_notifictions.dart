9604707b4ab01ec748afe385a2ec49dfaf1b91e3
AmirHossein <amirhossein1mirzaei79@gmail.com>
2024-05-27 08:41:45 -0700

    commit 2

--- lib/notification/local_notifictions.dart
+++ lib/notification/local_notifictions.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:rxdart/subjects.dart';
 
-import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
-
 class LocalNotification {
   static final FlutterLocalNotificationsPlugin
       _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
@@ -73,29 +70,29 @@ class LocalNotification {
     _flutterLocalNotificationsPlugin.cancel(id);
   }
 
-  static Future scheduleNotification({
-    required String title,
-    required String body,
-    required String payload,
-  }) async {
-    tz.initializeTimeZones();
-    await _flutterLocalNotificationsPlugin.zonedSchedule(
-        2,
-        title,
-        body,
-        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
-        const NotificationDetails(
-            android: AndroidNotificationDetails(
-                'channel 3', 'your channel name',
-                channelDescription: 'your channel description',
-                importance: Importance.max,
-                priority: Priority.high,
-                ticker: 'ticker')),
-        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime,
-        payload: payload);
-  }
+  // static Future scheduleNotification({
+  //   required String title,
+  //   required String body,
+  //   required String payload,
+  // }) async {
+  //   tz.initializeTimeZones();
+  //   await _flutterLocalNotificationsPlugin.zonedSchedule(
+  //       2,
+  //       title,
+  //       body,
+  //       tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+  //       const NotificationDetails(
+  //           android: AndroidNotificationDetails(
+  //               'channel 3', 'your channel name',
+  //               channelDescription: 'your channel description',
+  //               importance: Importance.max,
+  //               priority: Priority.high,
+  //               ticker: 'ticker')),
+  //       androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+  //       uiLocalNotificationDateInterpretation:
+  //           UILocalNotificationDateInterpretation.absoluteTime,
+  //       payload: payload);
+  // }
 
   static Future cancelAll() async {
     await _flutterLocalNotificationsPlugin.cancelAll();
